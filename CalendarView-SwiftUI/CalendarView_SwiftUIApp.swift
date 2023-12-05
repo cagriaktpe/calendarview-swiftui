f6e2df20d37bd27252a8b33b3609755d24d8cd77
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-05 11:21:21 +0300

    Initial Commit

---
+++ CalendarView-SwiftUI/CalendarView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarView_SwiftUIApp.swift
+//  CalendarView-SwiftUI
+//
+//  Created by Samet Çağrı Aktepe on 5.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalendarView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarView-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalendarView-SwiftUI
+//
+//  Created by Samet Çağrı Aktepe on 5.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
